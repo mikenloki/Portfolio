21038f48ce37eac41136d861674fcb2cc802ac57
mikenloki <mikenloki@aol.com>
2017-09-14 11:51:08 -0400

    Got NavBar up and running.

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -1,151 +1,29 @@
-/**
-*
-* NavBar
-*
-*/
+/*
+ *
+ * Home
+ *
+ */
 
 import React from 'react';
-import { Link } from 'react-router';
+import Helmet from 'react-helmet';
 
 import './style.css';
 import './styleM.css';
 
-import Bars from 'react-icons/lib/fa/bars';
+import NavBar from 'components/NavBar';
 
-export default class NavBar extends React.PureComponent {
-
-  constructor() {
-    super();
-    this.state = {
-      menuOpen:false
-    }
-  }
-
-  handleMenu = () => {
-    if(this.state.menuOpen === true)
-    {
-      this.setState({
-        menuOpen:false
-      })
-    }
-    else if(this.state.menuOpen === false)
-    {
-      this.setState({
-        menuOpen:true
-      })
-    }
-  }
-
-  renderMenu() {
-    if(this.state.menuOpen === true) {
-      return(
-        <nav className="navMobile">
-          <Link to="/" className="navButton">Home</Link>
-          <Link to="/contact"className="navButton">Contact</Link>
-          <a href="https://github.com/mikenloki" className="navButton">Github</a>
-        </nav>
-      )
-    }
-  }
-
+export default class Home extends React.PureComponent {
   render() {
     return (
-      <div>
-        <div className="navBar">
-          <div className="siteName">New Zion</div>
+      <div className="container">
+        <Helmet title="Home" meta={[ { name: 'description', content: 'Description of Home' }]}/>
+        <NavBar/>
 
-          <nav className="nav">
-            <Link to="/" className="navButton">Home</Link>
-            <Link to="/contact" className="navButton">Contact</Link>
-            <a href="https://github.com/mikenloki" className="navButton">github</a>
-          </nav>
-
-          <Bars className="menuIcon" onClick={this.handleMenu}/>
-
-        </div>
-        {this.renderMenu()}
       </div>
     );
   }
 }
 
-NavBar.contextTypes = {
-  router: React.PropTypes.object
-};
-
-==================================================
-
-/**
-*
-* NavBar
-*
-*/
-
-import React from 'react';
-import { Link } from 'react-router';
-
-import './style.css';
-import './styleM.css';
-
-import Bars from 'react-icons/lib/fa/bars';
-
-export default class NavBar extends React.PureComponent {
-
-  constructor() {
-    super();
-    this.state = {
-      menuOpen:false
-    }
-  }
-
-  handleMenu = () => {
-    if(this.state.menuOpen === true)
-    {
-      this.setState({
-        menuOpen:false
-      })
-    }
-    else if(this.state.menuOpen === false)
-    {
-      this.setState({
-        menuOpen:true
-      })
-    }
-  }
-
-  renderMenu() {
-    if(this.state.menuOpen === true) {
-      return(
-        <nav className="navMobile">
-          <Link to="/" className="navButton">Home</Link>
-          <Link to="/contact"className="navButton">Contact</Link>
-          <a href="https://github.com/mikenloki" className="navButton">Github</a>
-        </nav>
-      )
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="navBar">
-          <div className="siteName">New Zion</div>
-
-          <nav className="nav">
-            <Link to="/" className="navButton">Home</Link>
-            <Link to="/contact" className="navButton">Contact</Link>
-            <a href="https://github.com/mikenloki" className="navButton">github</a>
-          </nav>
-
-          <Bars className="menuIcon" onClick={this.handleMenu}/>
-
-        </div>
-        {this.renderMenu()}
-      </div>
-    );
-  }
-}
-
-NavBar.contextTypes = {
+Home.contextTypes = {
   router: React.PropTypes.object
 };
